2f5650cb27014c5bf67d8a240dccc8eb8fefded9
Djaysson <djayson.dev@gmail.com>
2021-02-26 18:28:34 -0300

    Complete challenge functionality completed

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,7 +3,7 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
 
 
   return (
@@ -27,7 +27,7 @@ export function ChallengeBox() {
             <button
               type='button'
               className={styles.challengeSucceededButton}
-
+              onClick={completeChallenge}
             >
               completei
             </button>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
+  completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -47,6 +48,23 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   function resetChallenge() {
     setActiveChallenge(null);
   }
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
 
   return (
     <ChallengesContext.Provider
@@ -58,7 +76,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         startNewChallenge,
         activeChallenge,
         resetChallenge,
-        experienceToNextLevel
+        experienceToNextLevel,
+        completeChallenge
       }}
     >
       {children}
